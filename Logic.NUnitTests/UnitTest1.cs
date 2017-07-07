d216e0c3f04f1599d6242fdf8379ce194552b9b3
sergey.ganoldov <sergey.ganoldov@gmail.com>
2017-07-07 18:20:57 +0300

    1

---
+++ Logic.NUnitTests/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Logic.NUnitTests
+{
+    public class UnitTest1
+    {
+        #region GCD
+        [TestCase(5, 15, 25, 30, 35, 40, 45, ExpectedResult = 5)]
+        [TestCase(2, 15, ExpectedResult = 1)]
+        [TestCase(5, 0, 25, ExpectedResult = 5)]
+        [TestCase(0, 0, 0, 0, ExpectedResult = 0)]
+        [TestCase(60, 15, 30, ExpectedResult = 15)]
+        [TestCase(-15, 25, ExpectedResult = 5)]
+        public int GCD_Success(params int[] a)
+        {
+            long time;
+            return Class1.GCD(out time, a);
+        }
+
+        [TestCase(1)]
+        [TestCase(new int[] { })]
+
+        public void GCD_ThrowArgumentExeption(params int[] a)
+        {
+            long time;
+            Assert.Throws<ArgumentException>(() => Class1.GCD(out time, a));
+        }
+        #endregion
+
+        #region BinaryGCD
+        [TestCase(5, 15, 25, 30, 35, 40, 45, ExpectedResult = 5)]
+        [TestCase(2, 15, ExpectedResult = 1)]
+        [TestCase(5, 0, 25, ExpectedResult = 5)]
+        [TestCase(0, 0, 0, 0, ExpectedResult = 0)]
+        [TestCase(60, 15, 30, ExpectedResult = 15)]
+        [TestCase(-15, 25, ExpectedResult = 5)]
+        public int BinaryGCD_Success(params int[] a)
+        {
+            long time;
+            return Class1.BinaryGCD(out time, a);
+        }
+
+        [TestCase(1)]
+        [TestCase(new int[] { })]
+
+        public void BinaryGCD_ThrowArgumentExeption(params int[] a)
+        {
+            long time;
+            Assert.Throws<ArgumentException>(() => Class1.BinaryGCD(out time, a));
+        }
+        #endregion
+    }
+}
---
+++ Logic/Class1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class Class1
+    {
+        public static int GCD(out long time, params int[] a)
+        {
+            Stopwatch t = new Stopwatch();
+            t.Start();
+
+            if (a.Length < 2) throw new ArgumentException($"{nameof(a)} must have 2 or more numbers");
+            int res = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                long time1;
+                res = GCD(out time1, res, a[i]);
+            }
+
+            time = t.ElapsedMilliseconds;
+            return res;
+        }
+
+        public static int GCD(out long time, int a, int b)
+        {
+            Stopwatch t = new Stopwatch();
+            t.Start();
+
+            while (b != 0)
+            {
+                int c = b;
+                b = a % b;
+                a = c;
+            }
+
+            time = t.ElapsedMilliseconds;
+            return Math.Abs(a);
+        }
+
+
+        public static int BinaryGCD(out long time, params int[] a)
+        {
+            Stopwatch t = new Stopwatch();
+            t.Start();
+
+            if (a.Length < 2) throw new ArgumentException($"{nameof(a)} must have 2 or more numbers");
+            int res = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                long time1;
+                res = BinaryGCD(out time1, res, a[i]);
+            }
+
+            time = t.ElapsedMilliseconds;
+            return res;
+        }
+
+        public static int BinaryGCD(out long time, int a, int b)
+        {
+            Stopwatch t = new Stopwatch();
+            t.Start();
+
+            if (a == b)
+            {
+                time = t.ElapsedMilliseconds;
+                return a;
+            }
+            if (a == 0)
+            {
+                time = t.ElapsedMilliseconds;
+                return b;
+            }
+            if (b == 0)
+            {
+                time = t.ElapsedMilliseconds;
+                return a;
+            }
+
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+
+            long time1;
+            if ((~a & 1) != 0)
+            {
+                if ((b & 1) != 0)
+                {
+                    time = t.ElapsedMilliseconds;
+                    return BinaryGCD(out time1, a >> 1, b);
+                }
+
+                time = t.ElapsedMilliseconds;
+                return BinaryGCD(out time1, a >> 1, b >> 1) << 1;
+
+            }
+            if ((~b & 1) != 0)
+            {
+                time = t.ElapsedMilliseconds;
+                return BinaryGCD(out time1, a, b >> 1);
+            }
+            if (a > b)
+            {
+                time = t.ElapsedMilliseconds;
+                return BinaryGCD(out time1, (a - b) >> 1, b);
+            }
+            time = t.ElapsedMilliseconds;
+            return BinaryGCD(out time1, (b - a) >> 1, a);
+
+        }
+
+    }
+}
